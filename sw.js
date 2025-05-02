7bc5a6aff2c1b9c17facb148bf48a213e72141d5
ko-izu <matsuitakeshi33@gmail.com>
2025-05-03 01:10:03 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,64 @@
-var CACHE_NAME = 'pwa-sample-cache-v2';
-var urlsToCache = [
-    '/index.html',
-    '/manifest.webmanifest',
-    '/sw.js',
-"https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.min.js",
-"https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.min.css",
-"https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js",
-"https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css",
-"https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css",
-"https://cdnjs.cloudflare.com/ajax/libs/nosleep/0.12.0/NoSleep.min.js",
+// バージョン定義
+var CACHE_VERSION = 'ca-v1';
+var DISP_VERSION = 'ca-d-v1';
+
+// キャッシュの対象にするディレクトリ（css/jsは個別で追加）
+var resources = [
+  '/',
+  '/img'
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+// キャッシュ追加
+self.addEventListener('install', function (event) {
+  console.log('ServiceWorker Install');
+  event.waitUntil(
+    caches.open(CACHE_VERSION)
+      .then(function (cache) {
+        console.log('cache.addAll');
+        cache.addAll(resources);
+      })
+  );
 });
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
+// キャッシュ表示
+self.addEventListener('fetch', function (event) {
+  console.log('ServiceWorker fetch');
+  event.respondWith(
+    // キャッシュが存在するかチェック
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        } else {
+          // キャッシュがない場合キャッシュに入れる
+          return fetch(event.request)
+            .then(function (res) {
+              return caches.open(DISP_VERSION)
+                .then(function (cache) {
+                  console.log('cache.put');
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                });
             })
-    );
+            .catch(function () {
+              // 何もしない
+            });
+        }
+      })
+  );
 });
-
+// 古いキャッシュを削除
+self.addEventListener('activate', function (event) {
+  console.log('activate ServiceWorker');
+  event.waitUntil(
+    caches.keys()
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+          if (key !== CACHE_VERSION && key !== DISP_VERSION) {
+            console.log('cache.delete');
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
